8bc224e57dbad0d0fb7ebdf56ac04ddd604c77a5
Mohd Harish <163094451+Hellkryptonium@users.noreply.github.com>
2026-05-07 01:57:07 +0530

    Refactor and enhance listing and review functionality; add middleware for validation and ownership checks, update models and views for user interactions

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -16,8 +17,13 @@ router.post(
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
-      req.flash("success", "Welcome to wanderlust!");
-      res.redirect("/listings");
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to wanderlust!");
+        res.redirect("/listings");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/signup");
@@ -31,14 +37,26 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "Welcome to Wanderlust! You are logged in!");
-    res.redirect("/listings");
+    let redirectURL = res.locals.redirectURL || "/listings";
+    res.redirect(redirectURL);
   },
 );
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      next(err);
+    }
+    req.flash("success", "you are logged out");
+    res.redirect("/listings");
+  });
+});
 
 module.exports = router;
