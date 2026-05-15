ed421dd32c52b2bc0aafc135063b4579b1f36bc6
Mohd Harish <163094451+Hellkryptonium@users.noreply.github.com>
2026-05-15 23:20:16 +0530

    feat: enhance listings page with filter options and tax display toggle

--- app.js
+++ app.js
@@ -68,19 +68,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/demouser", async (req, res) => {
-//   let fakeUser = new User({
-//     email: "student@gmail.com",
-//     username: "delta-student",
-//   });
-
-//   let registeredUser = await User.register(fakeUser, "helloworld");
-//   res.send(registeredUser);
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
 // });
-
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
 
 //Listing routes
 app.use("/listings", listingRouter);
